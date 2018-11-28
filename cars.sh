bd723136f7f6b591ab3ac036407078b7084732eb
Nicholas Mirchandani <nmirchandani@chapman.edu>
2018-11-28 13:47:56 -0800

    Updates script

--- cars.sh
+++ cars.sh
@@ -2,34 +2,57 @@
 #cars.sh
 #Nicholas Mirchandani
 
+clear #Clears are used to keep only relevant details on screen
 while true
 do
+	echo -e "COMMANDS"
 	echo -e --------------------
 	echo -e "1) Add a car"
 	echo -e "2) List the cars"
 	echo -e "3) Quit the Program"
 	echo -e --------------------
-	echo -e #blank echo for formatting
-	read command
+	echo -e #blank echo's are for formatting
+	read -n 1 -s -p "" command
+
+	echo -e
+
 	case "$command" in
     		"1") #Add a Car
+			clear
+			echo -e "ADDING A CAR"
+			echo -e "--------------------\n"
 			echo "What year is the car you'd like to add?"
-			read year
+			read -r year
 			echo "What is the make of the car you'd like to add?"
-			read make
+			read -r make
 			echo "What is the model of the car you'd like to add?"
-			read model
+			read -r model
 			echo $year:$make:$model >> My_old_cars
-			echo -e #blank echo for formatting
+			echo -e
+			echo "Congratulations!  Your car, a "$year" "$make" "$model", has been added!"
+			echo -e "\n--------------------\n"
+			read -n 1 -s -r -p "Press any key to return to menu..." #Requires 1 character to be pressed, hides user input, and doesn't care about special characters to allow the user to press any key to continue.
+			clear
 		;;
     		"2") #Lists the cars
-			sort My_old_cars | tr ':' ' '
-			echo -e #blank echo for formatting
+			clear
+			echo -e "LIST OF CARS"
+			echo "--------------------"
+			sort My_old_cars | tr ':' ' ' #sorts cars by year and then replaces colons with spaces.
+			echo -e "--------------------\n"
+			read -n 1 -s -r -p "Press any key to return to menu..." #Same as before
+			clear
 		;;
-    		"3") clear
-			echo -e "Goodbye!"
-			break
+    		"3") #Exits the program
+			clear
+			echo -e "FAREWELL"
+			echo -e "--------------------\n"
+			read -n 1 -s -r -p "Press any key to exit..." #Same as before
+			clear
+			break #breaks out of the while loop and ultimately ends the program.
 		;;
-     		*) echo "Sorry, I do not know what you want.";;
- esac
+     		*) #Default case
+			clear
+		;;
+	esac
 done
